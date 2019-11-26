1e65b3b89a4780861c6f613b58f3ccbcb3f865f8
Jaitl <Jaitl@outlook.com>
2019-11-26 16:29:53 +0300

    fix test (#18)

--- app/exercises/speech_test.go
+++ app/exercises/speech_test.go
@@ -12,11 +12,11 @@ func TestSpeechHandleCorrectAnswer(t *testing.T) {
 	assert.False(t, res.IsCorrectAnswer)
 	assert.Equal(t, float32(0.5), res.MatchScore)
 
-	res = speech.HandleAnswer("This tests whether a pattern matches the wrong")
+	res = speech.HandleAnswer("This tests")
 	assert.False(t, res.IsCorrectAnswer)
-	assert.Equal(t, float32(0.75), res.MatchScore)
+	assert.Equal(t, float32(0.25), res.MatchScore)
 
-	res = speech.HandleAnswer("This tests whether a pattern matches a wrong")
+	res = speech.HandleAnswer("This tests whether a pattern ttt a wrong")
 	assert.True(t, res.IsCorrectAnswer)
-	assert.Equal(t, float32(0.875), res.MatchScore)
+	assert.Equal(t, float32(0.75), res.MatchScore)
 }
